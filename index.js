8807a393c1800ca66bf436e30354b5f0aa55302a
ahmed6893 <rifatahmed6893@gmail.com>
2025-03-22 19:00:45 +0600

    if-else

---
+++ index.js
@@ -0,0 +1,18 @@
+let num = prompt("Enter your Mark")
+
+if(num >80)
+    console.log("A+")
+else
+    if(num>60)
+        console.log("A")
+    else
+        if(num>50)
+            console.log("A-")
+        else
+            if(num>44)
+                console.log("B")
+            else
+            if(num<30)
+                console.log("F +")
+            else ;
+
