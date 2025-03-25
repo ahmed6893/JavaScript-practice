cd2d16655c5932238f690e14b76ea07852c88a8a
ahmed6893 <rifatahmed6893@gmail.com>
2025-03-25 18:00:03 +0600

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 let num = prompt("Enter your Mark")
 
 if(num >80)
-    console.log("A+")
-else
-    if(num>60)
-        console.log("A")
-    else
-        if(num>50)
-            console.log("A-")
-        else
-            if(num>44)
-                console.log("B")
+    console.log("A+");
+else if(num>60)
+        console.log("A");
+    else if(num>50)
+            console.log("A-");
+        else if(num>44)
+                console.log("B");
+            else if(num<30)
+                console.log("F +");
             else
-            if(num<30)
-                console.log("F +")
-            else ;
+                console.log("F ++");
 
