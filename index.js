2a14faa8548001dce01b8c871b6e209e579b0edc
ahmed6893 <rifatahmed6893@gmail.com>
2025-04-14 21:08:08 +0600

    ternary

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-let letter = prompt("Enter a letter")
+let user = prompt("Enter Your Name");
+let salary =prompt("Enter Your Salary");
 
-letter =letter.toLowerCase();
+let expenses = prompt("Enter your Expenses");
 
-if(letter== "a" || letter=="e" || letter=="i" || letter=="o" || letter=="u")
+let tax = salary * 0.1 ;
+let netIncome = salary - tax ;
+let balance = netIncome - expenses;
+let savings = balance * 0.2 ;
+
+if (savings>= 1000)
 {
-    console.log("Vowel");
+    console.log(user + "Excelent Your saving well ->" + savings)
 }
-else{
-    console.log("Consonent");
+else if(savings>=500)
+{
+    console.log(user +"Your saving is decent ->" + savings)
+}
+else if(savings>=100)
+{
+    console.log(user +"Oh no Your saving arent very well ->" + savings) 
 }
---
+++ ternary.js
@@ -0,0 +1,59 @@
+// let number = Number(prompt("Enter Your Number"));
+// if(number>0){
+//     console.log("Positive")
+// }
+// else{
+//     console.log("Negative")
+// }
+
+// let result = number > 0 ? "Positive" : "Negative"
+// console.log(result);
+
+// let result = number>0 ? "Positive": number<0 ? "Negative" : "zero";
+// console.log(result);
+
+// switch (number)
+// {
+//     case "0":
+//         console.log("Zero");
+//         break
+//     case "1":
+//         console.log("One");
+//         break
+//     case "2":
+//         console.log("two");
+//         break
+//     case "3":
+//         console.log("three");
+//         break
+//     case "4":
+//         console.log("Four");
+//         break
+//     case "1":
+//         console.log("Five");
+//         break
+//     default:
+//         console.log("NOt a digit")
+// }
+let number = prompt("Enter Your Letter");
+
+switch(number)
+{
+        case "a":
+            console.log("Letter is vowel");
+            break
+        case "e":
+            console.log("Letter is vowel");
+            break
+        case "i":
+            console.log("Letter is vowel");
+            break
+        case "o":
+            console.log("Letter is vowel");
+            break
+        case "u":
+            console.log("Letter is vowel");
+            break
+            default:
+                console.log("Letter is Consonant")
+}
