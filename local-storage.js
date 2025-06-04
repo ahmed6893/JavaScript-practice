97718fe5377dc4d6554c0fc3de8eae322c338d5e
ahmed6893 <ahmedrifat6893@gmail.com>
2025-06-04 17:19:32 +0600

    practicing locaStorage and SessionStorage

--- local-storage.js
+++ local-storage.js
@@ -1,3 +1,19 @@
 // localStorage.setItem("user_name","Harun Khan");
 const userName= localStorage.getItem("user_name")
 console.log(userName);
+
+localStorage.setItem("user_name","Mia Khan");
+
+
+const countries = ["Japan","America","Borisal"];
+localStorage.setItem("countries",JSON.stringify(countries));
+
+const countriesList =JSON.parse(localStorage.getItem("countries"));
+console.log(countriesList);
+
+
+//sessionStorage 
+sessionStorage.setItem("user_name","Alu Khan");
+
+const userNames=sessionStorage.getItem("user_name");
+
