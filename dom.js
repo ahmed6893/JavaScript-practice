005fd65e5bf78f9d6a0ba9bef0ad3e488969a752
ahmed6893 <ahmedrifat6893@gmail.com>
2025-06-30 15:56:36 +0600

    practice

--- dom.js
+++ dom.js
@@ -1,3 +1,7 @@
 document.getElementById("heading1").innerText =" hello";
 document.getElementsByClassName("header").innerHTML="HI";
 document.getElementsByTagName("h1")[0].innerHTML="by"
+
+document.querySelector("#changeColorBtn").addEventListener("click",function(){
+    document.querySelector("#title").style.color="red"
+});
