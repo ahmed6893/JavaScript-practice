897d484030b4d303a6bb93b4e654381d7292e545
ahmed6893 <ahmedrifat6893@gmail.com>
2025-06-30 17:41:55 +0600

    practice

--- dom.js
+++ dom.js
@@ -5,3 +5,10 @@ document.getElementsByTagName("h1")[0].innerHTML="by"
 document.querySelector("#changeColorBtn").addEventListener("click",function(){
     document.querySelector("#title").style.color="red"
 });
+
+document.querySelectorAll(".mybutton").forEach(function(button) {
+    button.addEventListener("click", function() {
+        alert("Button clicked: " + this.innerText);
+    });
+});
+
